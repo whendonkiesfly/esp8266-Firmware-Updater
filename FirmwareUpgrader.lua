225a96834007b209f8a781ecb000d06bb193c0ef
Westin <westin@localhost.localdomain>
2016-04-04 20:21:43 -0500

    Fixed typo

--- FirmwareUpgrader.lua
+++ FirmwareUpgrader.lua
@@ -28,7 +28,7 @@ wifi.ap.config(apCfg)
 wifi.ap.setip(ipCfg)
 
 tmr.alarm(0, 1000, 1, function()
-	 if wifi.ap.getip() != nil then
+	 if wifi.ap.getip() ~= nil then
 		ip, netmask, gateway = wifi.ap.getip()
 		print("IP Address: " .. ip .. "\n")
 		print("Netmask: " .. netmask .. "\n")
